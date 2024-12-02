8792c5366694966a66b414284105c80e6bf86ba8
sawwere <sawer_13@mail.ru>
2024-12-02 16:07:28 +0300

    Add methods for user create/update

--- src/main/java/ru/sfedu/teamselection/controller/StudentController.java
+++ src/main/java/ru/sfedu/teamselection/controller/StudentController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.sfedu.teamselection.domain.User;
 import ru.sfedu.teamselection.dto.StudentCreationDto;
 import ru.sfedu.teamselection.dto.StudentDto;
 import ru.sfedu.teamselection.dto.TeamDto;
-import ru.sfedu.teamselection.dto.UserDto;
 import ru.sfedu.teamselection.mapper.StudentDtoMapper;
 import ru.sfedu.teamselection.mapper.TeamDtoMapper;
 import ru.sfedu.teamselection.service.StudentService;
@@ -153,17 +150,6 @@ public class StudentController {
     public void deleteStudent(@PathVariable(value = "id") Long studentId) {
         LOGGER.info("ENTER deleteStudent(%d) endpoint".formatted(studentId));
         studentService.delete(studentId);
-    }
-
-    /**
-     * Получение текущего пользователя.
-     * @return информация о текущем пользователе.
-     */
-    @GetMapping("/api/v1/me")
-    public ResponseEntity<UserDto> getCurrentUser() {
-        User currentUser = userService.getCurrentUser();
-        UserDto userDto = new UserDto(currentUser.getId(), currentUser.getFio(), currentUser.getEmail(), "test");
-        return ResponseEntity.ok(userDto);
     }
 
     /**
--- src/main/java/ru/sfedu/teamselection/dto/UserDto.java
+++ src/main/java/ru/sfedu/teamselection/dto/UserDto.java
@@ -1,11 +1,14 @@
 package ru.sfedu.teamselection.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Value;
 
 /**
  * DTO for {@link ru.sfedu.teamselection.domain.User}
@@ -16,8 +19,25 @@ import lombok.Value;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-    Long id;
-    String fio;
-    String email;
-    String role;
+    @Min(1)
+    private Long id;
+
+    @NotBlank
+    private String fio;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String role;
+
+    @JsonProperty(value = "is_remind_enabled")
+    @NotNull
+    @Builder.Default
+    private Boolean isRemindEnabled = Boolean.TRUE;
+
+    @JsonProperty(value = "is_enabled")
+    @NotNull
+    @Builder.Default
+    private Boolean isEnabled = Boolean.TRUE;
 }
