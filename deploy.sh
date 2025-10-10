209032e4b423a74265d0704296173434d4b7e46f
sawwere <sawer_13@mail.ru>
2025-10-10 23:26:22 +0300

    feat: fix is_full when update team, improve deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 
 set -e  # Прерывать выполнение при ошибке любой команды
 
+echo "Stopping team-selection container..."
 docker compose stop team-selection
-docker rm team-selection || echo "Контейнер team-selection не существует или уже удален"
-docker image rm team-selection-team-selection || echo "Образ team-selection-team-selection не существует или уже удален"
+docker rm team-selection || echo "Container team-selection does not exist or already removed"
+docker image rm team-selection-team-selection || echo "team-selection-team-selection image does not exist or already removed"
+echo "Building new image..."
 docker compose build --no-cache team-selection
+echo "Starting container..."
 docker compose up -d team-selection
 
-echo "Скрипт завершен успешно"
+echo "Script completed successfully"
